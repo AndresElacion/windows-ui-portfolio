071d3215e2ce24f85f9da984d1f8cf9566210f42
AndresElacion <andreselacioniii@gmail.com>
2024-12-03 14:03:19 +0800

    initial commit

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import { Head, Link } from '@inertiajs/react';
+
+export default function Welcome() {
+
+    return (
+        <>
+            <Head title="Welcome" />
+
+            
+        </>
+    );
+}
